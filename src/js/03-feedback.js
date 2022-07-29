b5891fa393f88db0ba03f74fbbaf4909305ec03f
Yuliia Ovsiannikova <yuliia.ovsiannikova@outlook.com>
2022-07-29 23:53:47 +0200

    Completed task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORE_KEY = 'feedback-form-state';
+const dataObj = {};
+onPageLoad();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+	dataObj.email = form.elements.email.value;
+	dataObj.message = form.elements.message.value;
+	localStorage.setItem(LOCALSTORE_KEY, JSON.stringify(dataObj));
+}
+
+function onFormSubmit(e) {
+	e.preventDefault();
+	e.currentTarget.reset();
+	localStorage.removeItem(LOCALSTORE_KEY);
+	console.log(dataObj);
+}
+
+function onPageLoad() {
+	const fetchedData = JSON.parse(localStorage.getItem(LOCALSTORE_KEY));
+	if (fetchedData) {
+		form.elements.email.value = fetchedData.email;
+		form.elements.message.value = fetchedData.message;
+	}
+}
