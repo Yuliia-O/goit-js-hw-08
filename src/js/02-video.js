8e1b21fd7902f5db64f73b267024840e6363f854
Yuliia Ovsiannikova <yuliia.ovsiannikova@outlook.com>
2022-07-31 19:41:08 +0200

    Completed task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerEl = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const vPlayer = new Player(playerEl);
+
+onLoad();
+vPlayer.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(e) {
+	localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+}
+
+function onLoad(e) {
+	const fetchedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+	if (LOCALSTORAGE_KEY) {
+		vPlayer.setCurrentTime(fetchedTime);
+	}
+}
